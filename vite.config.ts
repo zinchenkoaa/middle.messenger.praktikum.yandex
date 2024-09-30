12358319185d35353f104cbf3205e86479d3b4f6
1111 <a1111@MacBook-Air-1111.local>
2024-09-30 23:06:08 +0300

    Продолжение настройки проекта

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,28 @@
-import { defineConfig } from 'vite';
+import { defineConfig, PluginOption } from 'vite';
+import handlebars from 'vite-plugin-handlebars';
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   root: 'src',
   server: {
     port: 3000
   },
+  plugins: [
+    handlebars({
+        partialDirectory: resolve(__dirname, 'src', 'partials'),
+        context: {
+          title: 'Sparkit Messenger',
+        },
+        compileOptions: {},
+        runtimeOptions: {},
+        helpers: {},         
+        reloadOnPartialChange: true,
+      }) as PluginOption,
+  ],
   build: {
     outDir: '../dist',
     emptyOutDir: true
