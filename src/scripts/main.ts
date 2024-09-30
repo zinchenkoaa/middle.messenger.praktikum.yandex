8386d381470e40fa6a358342b3d5d8e0013dd665
1111 <a1111@MacBook-Air-1111.local>
2024-09-30 18:09:52 +0300

    Настройка проекта

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,13 +1,9 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
+import '../style.css'
+import typescriptLogo from '../typescript.svg'
 import { setupCounter } from '../counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
